3b83e90738b764b2deb92d102ab9d037c6ef312f
playbooksolitude <slatopr@gmail.com>
2023-03-12 19:32:13 +0900

    .

--- 23-0309_word_cloud.R
+++ 23-0309_word_cloud.R
@@ -23,7 +23,7 @@ flights |> group_by(dest) |>
 
 #예쁜 컬러 선택
 brewer.pal.info  #저는 "Spectral", 11가지 색
-color <- brewer.pal(11,"Spectral")
+color <- brewer.pal(9,"Set1")
 
 #워드 클라우드 만들기
 wordcloud(words = word_cloud2$dest,
@@ -36,4 +36,90 @@ wordcloud(words = word_cloud2$dest,
           colors = color,
           use.r.layout = T)
 
-#good
+#billboard
+(billboard |> pivot_longer(cols = starts_with("wk"),
+                          values_to = "rank",
+                          names_to = "weeks") -> billboard_2_pivot)
+
+#group_by datasets
+billboard_2_pivot |> group_by(artist) |> summarise(n = n()) -> billboard_3
+
+#wordcloud
+wordcloud(words = billboard_3$artist,
+          freq = billboard_3$n,
+          min.freq = 10,
+          max.words = 300,
+          random.order = F,
+          rot.per = .1,
+          scale = c(4,0.3),
+          use.r.layout = T,
+          colors = color)
+
+color <- brewer.pal(8,"Dark2")
+brewer.pal.info
+
+#1등 많이 한 사람
+(billboard_2_pivot |> filter(rank == 1) |> group_by(artist) |> 
+  summarise(n = n()) |> arrange(desc(n)) -> billboard_3_no1)
+
+wordcloud(words = billboard_3_no1$artist,
+          freq = billboard_3_no1$n,
+          min.freq = 1,
+          max.words = 100,
+          random.order = F,
+          rot.per = .0,
+          scale = c(4,1),
+          use.r.layout = F,
+          colors = color)
+
+#top10
+(billboard_2_pivot |> filter(rank %in% c(1:10)) |> group_by(artist) |> 
+    summarise(n = n()) |> arrange(desc(n)) -> billboard_3_top10)
+
+wordcloud(words = billboard_3_top10$artist,
+          freq = billboard_3_top10$n,
+          min.freq = 1,
+          max.words = 100,
+          random.order = F,
+          rot.per = .0,
+          scale = c(3,.5),
+          use.r.layout = F,
+          colors = color)
+
+billboard
+(billboard_2_pivot |> filter(rank %in% c(1:10)) |> group_by(track) |> 
+    summarise(n = n()) |> arrange(desc(n)) -> billboard_3_track)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
