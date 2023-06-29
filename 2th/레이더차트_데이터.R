53b23b2ad0f01fe41891474e9a0f3bfb1503825f
playbooksolitude <slatopr@gmail.com>
2023-06-29 13:52:21 +0900

    .

--- 2th/레이더차트_데이터.R
+++ 2th/레이더차트_데이터.R
@@ -1,91 +1,49 @@
-#23-0629 
+#23-0629 thu 1)00
 
 #
-install.packages("fmsb")
+#install.packages("fmsb")
 library(fmsb)
+library(showtext)
+showtext_auto()
 
 #1 csv
-(read_tsv("./files/csv/thxdata3.tsv") |> 
-  select(-1) -> thx1_csv)
+(read_tsv("./2th/files/data_job8.tsv") -> thx4_csv)
+    
+#Analytics
+thx4_csv |> slice(1,2,4) |> 
+  radarchart(pcol="#F76461", pfcol = "#F7646180",
+             vlcex = 2,
+             title = "Data Analytics") 
 
-  #factor
-as.factor(thx1_csv$type) -> thx1_csv$type
+#Engineer
+thx4_csv |> slice(1:3) |> 
+  radarchart(pcol="#1B9E77", pfcol = "#1B9E7780",
+             title = "Data Engineer",
+             vlcex = 2)   
 
-  #slice
-(thx1_csv |> slice(1) -> thx2_engineers)
-(thx1_csv |> slice(2) -> thx2_analysts)
-(thx1_csv |> slice(3) -> thx2_scientists)
-(thx1_csv |> slice(4) -> thx2_MLengineers)
+#Scientist
+thx4_csv |> slice(1,2,5) |> 
+  radarchart(pcol="#E79C54", pfcol = "#E79C5480",
+             title = "Data Scientist",
+             vlcex = 2) 
 
+#ML engineer
+thx4_csv |> slice(1,2,6) |> 
+  radarchart(pcol="#847574", pfcol = "#84757480",
+             title = "ML Engineer",
+             vlcex = 2) 
 
-  #marrix
-data_max <- c(10)
-data_min <- c(1)
-radarchart(thx1_csv)
-radarchart(thx2_analysts)
-
-#2 csv_한개
-(read_tsv("./files/csv/thxdata3_re.tsv") -> thx2_csv)
-radarchart(thx2_csv)
-
-#3 csv 합치기
-(read_tsv("./files/csv/thxdata3_re1.tsv") -> thx3_csv)
-radarchart(thx3_csv, 
-           title = "Data")
-
-?radarchart
-
-  #3-1
-thx3_csv |> 
-  radarchart(pfcol=topo.colors(4), pcol=topo.colors(4), pdensity = c(5,20))
-thx3_csv |> slice(1:3) |> radarchart()   #engineer
-thx3_csv |> slice(1,2,4) |> radarchart() #analytics
-thx3_csv |> slice(1,2,5) |> radarchart() #scientist
-thx3_csv |> slice(1,2,6) |> radarchart() #ML engineer
-
-dat <- data.frame(
-  total=runif(3, 1, 5),
-  phys=rnorm(3, 10, 2),
-  psycho=c(0.5, NA, 3),
-  social=runif(3, 1, 5),
-  env=c(5, 2.5, 4))
-dat
-(dat <- rbind(maxmin,dat))
-op <- par(mar=c(1, 2, 2, 1),mfrow=c(2, 2))
-op
-rm(op)
-
-radarchart(dat, axistype=1, seg=5, plty=1, vlabels=c("Total\nQOL", "Physical\naspects", 
-                                                     "Phychological\naspects", "Social\naspects", "Environmental\naspects"), 
-           title="(axis=1, 5 segments, with specified vlabels)", vlcex=0.5)
-
-radarchart(dat, axistype=2, pcol=topo.colors(3), plty=1, pdensity=c(5, 10, 30), 
-           pangle=c(10, 45, 120), pfcol=topo.colors(3), 
-           title="(topo.colors, fill, axis=2)")
-
-radarchart(dat, axistype=3, pty=32, plty=1, axislabcol="grey", na.itp=FALSE,
-           title="(no points, axis=3, na.itp=FALSE)")
-
-radarchart(dat, axistype=1, plwd=1:5, pcol=1, centerzero=TRUE, 
-           seg=4, caxislabels=c("worst", "", "", "", "best"),
-           title="(use lty and lwd but b/w, axis=1,\n centerzero=TRUE, with centerlabels)")
-par(op)
-
-
-
-thx3_csv |> 
-  radarchart(pfcol = colors_in, 
-             pcol = coul)
-
-thx3_csv |> slice(1:3) |> 
-  radarchart(pcol="#1B9E77", pfcol = "#1B9E7780")   #engineer
-thx3_csv |> slice(1,2,4) |> radarchart() #analytics
-thx3_csv |> slice(1,2,5) |> radarchart() #scientist
-thx3_csv |> slice(1,2,6) |> radarchart() #ML engineer
-
+#컬러에 알파값을 적용하고 
 library(RColorBrewer)
 brewer.pal.info
 coul <- brewer.pal(4, "Set3")
 (colors_border <- coul)
 library(scales)
 (colors_in <- alpha(coul, 0.5))
+
+
+#화면 분할
+op <- par(mar=c(1, 2, 2, 1),mfrow=c(2, 2))
+op
+#화면 분할 해제
+dev.off()
