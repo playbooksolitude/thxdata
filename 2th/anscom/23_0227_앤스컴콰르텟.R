b8c176e37e926d336fa8e796c8ad4a17aaa6a21e
playbooksolitude <slatopr@gmail.com>
2023-12-20 01:09:54 +0900

    ..

--- 2th/anscom/23_0227_앤스컴콰르텟.R
+++ 2th/anscom/23_0227_앤스컴콰르텟.R
@@ -4,58 +4,60 @@
 library(tidyverse)
 library(bbplot)
 library(patchwork)
+library(gt)
 
-#anscombe
 data()
 
 
+# 1 데이터 조회 ----
+anscombe |> gt()
 
-# --------------------------- dataset
-tibble(
-  x1 = anscombe$x,
-  y1 = anscombe$y
-) -> anscombe_1
+# 2 데이터 나눠 담기 ----
+(tibble(
+  x1 = anscombe$x1,
+  y1 = anscombe$y1) -> anscombe_1)
 
-tibble(
-  x1 = anscombe$x.1,
-  y1 = anscombe$y.1
-) -> anscombe_2
+(tibble(
+  x1 = anscombe$x2,
+  y1 = anscombe$y2) -> anscombe_2)
 
-tibble(
-  x1 = anscombe$x.2,
-  y1 = anscombe$y.2
-) -> anscombe_3
+anscombe
 
-tibble(
-  x1 = anscombe$x.3,
-  y1 = anscombe$y.3
-) -> anscombe_4
+(tibble(
+  x1 = anscombe$x3,
+  y1 = anscombe$y3) -> anscombe_3)
+
+(tibble(
+  x1 = anscombe$x4,
+  y1 = anscombe$y4) -> anscombe_4)
 
 anscombe_1;anscombe_1;anscombe_3;anscombe_4
 
-# --------------------------- ggplot
-(anscombe_1 |> 
+# ggplot ----
+(anscombe_1plot <- anscombe_1 |> 
   ggplot(aes(x = x1, y = y1)) + 
   geom_point(size = 5) + 
   geom_smooth(method = "lm", se = F) + 
-  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a1)
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)))
 
-anscombe_2 |> 
+(anscombe_2plot <- anscombe_2 |> 
   ggplot(aes(x = x1, y = y1)) + geom_point(size = 5) + 
   geom_smooth(method = "lm", se = F) +
-  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a2
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)))
 
-anscombe_3 |> 
+(anscombe_3plot <- anscombe_3 |> 
   ggplot(aes(x = x1, y = y1)) + geom_point(size = 5) + 
   geom_smooth(method = "lm", se = F) +
-  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a3
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)))
 
-anscombe_4 |> 
+(anscombe_4plot <- anscombe_4 |> 
   ggplot(aes(x = x1, y = y1)) + geom_point(size = 5) + 
   geom_smooth(method = "lm", se = F) +
-  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a4
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)))
 
-(a1 | a2) / (a3 | a4)
+(anscombe_1plot | anscombe_2plot) / (anscombe_3plot | anscombe_4plot)
+
+#
 ggsave("anscombe_patchwork.png")
 
 
